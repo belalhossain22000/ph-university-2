c3d07af2dd6cdcb8c3c8b665699d2894375d0d94
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-12-07 01:48:42 +0600

    make validate befor create adn upate time and delte  and get single department

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,4 +18,25 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>({
     timestamps: true
 })
 
+//check before posting data is exist or not exist
+academicDepartmentSchema.pre("save", async function (next) {
+    const isDepartmentExist = await AcademicDepartmentModel.findOne({ name: this.name })
+
+    if (isDepartmentExist) {
+        throw new Error("Department  is Already Exist")
+    }
+    next()
+})
+
+//check before update data is exist or not exist
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+    const query = this.getQuery()
+    const isDepartmentExist = await AcademicDepartmentModel.findOne(query)
+
+    if (!isDepartmentExist) {
+        throw new Error("Department  Dose not Exist")
+    }
+    next()
+})
+
 export const AcademicDepartmentModel = model<TAcademicDepartment>("Academic-Department", academicDepartmentSchema)
