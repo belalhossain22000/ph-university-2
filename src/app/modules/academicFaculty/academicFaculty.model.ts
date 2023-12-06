8464ef72007034dbdd4c951bffc71c90860cb380
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-12-06 21:04:13 +0600

    added academic faculty and validation schema and interface

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+import { TAcademicFaculty } from "./academicFaculty.interface";
+
+
+const academicFacultySchema = new Schema<TAcademicFaculty>({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+export const AcademicFacultyModel = model<TAcademicFaculty>("AcademicFaculty", academicFacultySchema)
