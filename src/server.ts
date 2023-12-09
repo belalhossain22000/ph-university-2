11e6002f41dec8f7f0304bfd02235202d518002e
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-12-09 13:23:30 +0600

    added unhandle and uncoaught rejecttion hanlder

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
 
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
@@ -15,3 +17,19 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+// 
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
